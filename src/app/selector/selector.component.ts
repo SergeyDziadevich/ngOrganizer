860135d4c1e286f461e20cd37d8b0c67335266da
ÐSergey Dziadevich <developersiteweb@gmail.com>
2020-04-12 16:08:13 +0300

    CalendarComponent template and styles

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -8,7 +8,7 @@ import {DateService} from '../shared/date.service';
 })
 export class SelectorComponent implements OnInit {
 
-  constructor(private dateService: DateService) { }
+  constructor(public dateService: DateService) { }
 
   ngOnInit(): void {
   }
